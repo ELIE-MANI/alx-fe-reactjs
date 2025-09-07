09888e1852cecedc58c23e214b0a695e22eb7503
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-07 12:51:15 +0200

    add UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './Header.jsx'
+import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
-
+import UserProfile from './components/UserProfile.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,9 +34,11 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
        <WelcomeMessage />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <Header />
        <MainContent />
         <Footer />
+
     </>
 
 
