08044a3a53a2f5aa1385127583b2576d512e2818
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-14 20:00:33 +0200

    add context API

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,13 @@
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <UserInfo/>
+
+      </div>
+  );
+}
+
+export default ProfilePage;
