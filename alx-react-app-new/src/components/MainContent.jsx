e7cc483de64e95418d91606a81d1f42875bff164
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-14 16:53:11 +0200

    add inline css

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{backgroundColor: 'aqua',fontSize: '16px',color: 'black', fontWeight: 'bold'}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
