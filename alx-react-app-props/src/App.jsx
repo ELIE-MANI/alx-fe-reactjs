8f09cd6f6e455f68601e381c720329994c3a7ee6
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-14 22:33:41 +0200

    refactor: replace prop drilling with Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import ProfilePage from './component/ProfilePage';
-import UserContext from './component/UserContext';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <>
-    <UserContext.Provider value={userData}/>
-      <ProfilePage userData={userData} />
-  </>
-
-  )
-  
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
-
-
