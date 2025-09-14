616afad7debb94c5f4548493b6fea813b2496bfd
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-14 21:30:05 +0200

    some changes

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -6,9 +6,10 @@ function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage/>
-    </UserContext.Provider>
+    <>
+    <UserContext.Provider value={userData}/>
+      <ProfilePage userData={userData} />
+  </>
 
   )
   
--- alx-react-app-props/src/component/UserContext.js
+++ alx-react-app-props/src/component/UserContext.js
@@ -1,3 +1,3 @@
-import { createContext } from "react";
-const UserContext = createContext();
+import React from 'react';
+const UserContext = React.createContext();
 export default UserContext;
