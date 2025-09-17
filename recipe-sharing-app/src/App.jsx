a8f6b3c21e59c4317bb2861c331206bba4111431
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-17 13:49:36 +0200

    add router to app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,28 @@
+import { BrowserRouter,Routes,Route } from "react-router";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
-
+import RecipeDetails from "./components/RecipeDetails";
 function App() {
   return ( 
-  <>
+ 
+  <BrowserRouter>
+<Routes>
+<Route path='/' element={
+ <>
   <h1>My Recipes</h1>
 <RecipeList />
 <AddRecipeForm />
+   </>
+}/>
+<Route path='/recipe/:id' element={<RecipeDetails />}/>
+</Routes>
 
-  </>
+
+  <h1>My Recipes</h1>
+<RecipeList />
+<AddRecipeForm />
+</BrowserRouter>
+
 
 
    );
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -2,17 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import RecipeDetails from './components/RecipeDetails.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter > 
-    <Routes >
-      <Route path='/' element={<App/>} />
-      <Route path='/recipe/:id' element={<RecipeDetails />}/>
-
-    </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
