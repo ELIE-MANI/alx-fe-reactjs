0d96732a0d4d2a8511f8851a3e94b462b79b4d82
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-24 16:54:27 +0200

    add tailwind css

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <>
     <Search />
+    <h1 className="text-4xl font-bold text-red-500">
+  Tailwind is working 🎉
+</h1>
+
     <h1>Github user search</h1>
     <p>Welcome</p>
     </>
