a30eadf6a009d7cd2d9650d356841557d70ba5a6
ELIE-MANI <eliemaniraguha@gmail.com>
2025-09-24 13:00:58 +0200

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -7,7 +7,9 @@ const [username, setUserName]= useState('');
 const [data, setUserData]= useState(null)
 const [loading, setLoading]= useState(false)
 const [error, setError]= useState(false)
-
+//updating the state
+const [location, setLocation]= useState('')
+const [minRepos, setMinRepos]= useState('')
 
  async function handleSubmit(e) {
   e.preventDefault();
@@ -26,29 +28,56 @@ const [error, setError]= useState(false)
 
   return (
     <>
-   <form onSubmit={handleSubmit}>
+   <form onSubmit={handleSubmit}  className="p-4 bg-white rounded shadow space-y-4">
     <input type="text" 
     placeholder="Enter GitHub username"
     value={username} 
     required 
-    onChange={e => setUserName(e.target.value)}/>
-    <button type="submit">Search</button>
+    onChange={e => setUserName(e.target.value)}
+    className="border p-2 rounded w-full"
+    />
+
+   <input type="text"
+   value={location}
+   placeholder="Location"
+   onChange={e => setLocation(e.target.value)}
+    className="border p-2 rounded w-full"
+    min="0"
+   />
+
+   <input type="number"
+   value={minRepos}
+   placeholder="minirepos"
+   onChange={e => setMinRepos(e.target.value)}
+      className="border p-2 rounded w-full"
+    min="0"
+   />
+
+    <button type="submit"
+     className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+    >Search</button>
    </form>
     {/* Loading state */}
     {loading && <p>loading...</p>}
      {/* Error state */}
     {error && <p>Looks like we cant find the user</p>}
 
-    {data && (
+    {data && data.length > 0 && (
      <> 
-     <div>
-      <img src={data.avatar_url} alt={data.login} />
-     </div>
+     <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+      {data.map(user => (
+      <div key={user.id} className="p-4 bg-white rounded shadow flex items-center gap-4" >
+      <img src={data.avatar_url} alt={data.login} className="w-16 h-16 rounded-full" />
+    
     <div>
-      <h2>{data.name || data.login}</h2>
-      <a href={data.html_url} target="_blank" rel="noreferrer">View Github Profile</a>
+      <h2 className="font-bold text-lg">{data.name || data.login}</h2>
+      <a href={data.html_url} target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">
+        View Github Profile</a>
 
       </div>
+      </div>
+      ))}
+       </div>
       </>
     )}
 </>
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
-export async function fetchUserData(username) {
+export async function fetchUserData(username,location, miniRepos) {
  try{
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
+  let query = '';
+  if (username) query += `${username}`;
+  if (location) query += `location:${location}`;
+  if (miniRepos) query += `repo:>=${miniRepos}`
+  const response = await axios.get(`https://api.github.com/users?q=${query}`);
+  return response.data.items;
  }
  catch (error){
   console.error('error fetching user:', error);
-  return null;
+  return [];
   
 
  }
