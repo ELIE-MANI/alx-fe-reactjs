32aa8c0e5b55e1a80d544314a947872c37924f33
ELIE-MANI <eliemaniraguha@gmail.com>
2025-10-09 11:13:07 +0200

    feat: setup and install react router

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
-    <HomePage/>
+    <BrowserRouter>
+    <Router>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/recipe/id" element={<RecipeDetails/>}/>
+    </Routes>
+    </Router>
+    </BrowserRouter>
+    
     </>
     );
 }
