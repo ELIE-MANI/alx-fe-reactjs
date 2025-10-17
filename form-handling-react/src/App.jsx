9781d8c81cb8edbf8d51ad88e8bad2314d4799df
ELIE-MANI <eliemaniraguha@gmail.com>
2025-10-17 11:07:47 +0200

    Fix file casing for FormikForm.js

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,12 +1,10 @@
-
-import { Formik } from "formik";
 import RegistrationForm from "./components/RegistrationForm";
 
 
 function App() {
   return ( <>
 <RegistrationForm/>
-<Formik/>
+
   </> );
 }
 
