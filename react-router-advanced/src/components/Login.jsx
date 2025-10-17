b2d1c70ca8df54c703012f8817c80f745b795083
ELIE-MANI <eliemaniraguha@gmail.com>
2025-10-17 13:52:10 +0200

    feat: add protext route for profile

---
+++ react-router-advanced/src/components/Login.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+localStorage.setItem("auth","true");
+navigate("/profile")
+  }
+  
+  return ( 
+    <>
+     <h1>Login Page</h1>
+    <button onClick={handleLogin}>Login</button>
+    </>
+   );
+}
+
+export default Login;
